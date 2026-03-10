62505c0d9fb1a1cac30e40cd2bc4d879b69221b2
yodoshia <136631690+yodoshia@users.noreply.github.com>
2026-03-10 14:38:44 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,9 @@
+console.log("Endereço Completo");
+
+let rua = "Rua das Flores";
+let numero = 489;
+let cidade = "Curitiba";
+
+let endereco = rua + "," + numero + " - " + cidade;
+//console.log(endereco);
+console.log("o endereço correto é:" + endereco);
