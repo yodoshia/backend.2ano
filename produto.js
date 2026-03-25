0ca7e842900e1f371d19fe28433c6d9c15a62a2b
yodoshia <136631690+yodoshia@users.noreply.github.com>
2026-03-25 18:28:22 +0000

    v1

---
+++ produto.js
@@ -0,0 +1,9 @@
+const produto = "Mochila Impermeável";
+let preco = 189.90;
+let estoque = 14; 
+const categoria = "acessório";
+
+console.log(`O produto é; ${produto}.`);
+console.log(`A ${produto} custa R$ ${preco.toFixed(2)}.`);
+console.log(`O estoque ainda possui ${estoque} mochila.`);
+console.log(`A categoria de ${produto} é ${categoria}.`);
