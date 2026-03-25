11d0226cb60149fac9e987c461e468427e8888d4
yodoshia <136631690+yodoshia@users.noreply.github.com>
2026-03-25 18:48:06 +0000

    v3

--- temperatura.js
+++ temperatura.js
@@ -1,6 +1,10 @@
 //conversor de temperatura
 const cidade = "Curitiba";
-let tempCelsius = 18.5;
+let tempCelsius = 26;
 
 const tempFahrenheit = tempCelsius * (9/5) + 32;
 const tempkenvin = tempCelsius + 273.15;
+
+console.log(`A tempratura de ${cidade} está ${tempCelsius} Cº`);
+console.log(`Fahrenheit: ${tempFahrenheit.toFixed(1)} fº`);
+console.log(`Kelvin: ${tempkenvin.toFixed(1)} Kº`);
