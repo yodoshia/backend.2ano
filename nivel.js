d40a27677382d054d6eb1c13f01863644171da5d
yodoshia <136631690+yodoshia@users.noreply.github.com>
2026-04-14 17:33:10 +0000

    v1

---
+++ nivel.js
@@ -0,0 +1,9 @@
+function subirDeNivel(nivelAtual){
+    return nivelAtual + 1;
+};
+
+let meuNivel = 10;
+let nivelAtualizado = subirDeNivel(meuNivel);
+
+
+console.log("Seu nivel atual é:" + nivelAtualizado);
