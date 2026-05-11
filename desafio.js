0073daac116a9aa79bdbb622df6376d5d94a8b68
yodoshia <136631690+yodoshia@users.noreply.github.com>
2026-05-11 19:10:45 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,9 @@
+console.log ("PerfildoJogador")
+
+let NomeJogador = "Neymar"
+let Posicao = "lateral esquerda"
+let IdadeJogador = "34 anos"
+let Time = "Barcelo"
+let NumeroCamisa = "10"
+
+console.log(`O jogador ${NomeJogador}, joga pela ${Posicao} e tem ${IdadeJogador}, jogando pelo ${Time} com a camisa ${NumeroCamisa}.`);
