5a573026ce96ef6e14a5ebb558762300cfb975f4
yodoshia <136631690+yodoshia@users.noreply.github.com>
2026-05-18 19:20:25 +0000

    v33

--- desafio.js
+++ desafio.js
@@ -1,9 +1,15 @@
 console.log ("PerfildoJogador")
 
-let NomeJogador = "Neymar"
-let Posicao = "lateral esquerda"
-let IdadeJogador = "34 anos"
-let Time = "Barcelo"
-let NumeroCamisa = "10"
+let nomeJogador = "Neymar"
+let posicao = "lateral esquerda"
+let idadeJogador = "34 anos"
+let temime = "Barcelo"
+let numeroCamisa = "10"
+
 
 console.log(`O jogador ${NomeJogador}, joga pela ${Posicao} e tem ${IdadeJogador}, jogando pelo ${Time} com a camisa ${NumeroCamisa}.`);
+
+let Salario = "4.500000"
+console.log("Salario mensal = 4.500000")
+let 
+let soma = (Salario +
